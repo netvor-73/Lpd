827c1b4ef962e0ca4d9877b1b7dccf47ddaad8df
Malik <malik.kissoum@gmail.com>
2020-08-09 10:57:49 +0100

    iou\'s implementation added

--- model/utils.py
+++ model/utils.py
@@ -147,3 +147,27 @@ def yolo_eval(yolo_output):
     scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes)
 
     return scores, boxes, classes
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+    
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_area = max((yi2 - yi1), 0) * max((xi2 - xi1), 0)
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    box1_area = (box1[3] - box1[1]) * (box1[2] - box1[0])
+    box2_area = (box2[3] - box2[1]) * (box2[2] - box2[0])
+    union_area = box1_area + box2_area - inter_area
+
+    # compute the IoU
+    iou = inter_area / union_area
+    return iou
